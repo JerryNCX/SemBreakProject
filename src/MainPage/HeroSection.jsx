e5188ceadd6bd4b81304b9d31335926c2c9810c4
JerryNCX <ncx030715@gmail.com>
2025-03-11 22:03:18 +0800

    HeroSection Update

--- src/MainPage/HeroSection.jsx
+++ src/MainPage/HeroSection.jsx
@@ -1,15 +1,37 @@
-import { Box, Button, Container, Flex, HStack, Stack, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Container,
+  Flex,
+  HStack,
+  Stack,
+  Text,
+  VStack,
+  Image,
+} from "@chakra-ui/react";
 import React from "react";
 
 export default function HeroSection() {
   return (
     <>
       <Flex bg="cyan.700">
-        <Stack align="start">
-        <Text fontSize="4xl">Get More Done With</Text>
-        <Text fontSize="4xl">whitespace</Text>
-        <Button bg="blue" color="white" mt="15%">Try Whitespace free</Button>
-        </Stack>
+        <HStack gap="">
+          <Stack align="start">
+            <Text fontSize="4xl">Get More Done With</Text>
+            <Text fontSize="4xl">whitespace</Text>
+            <Button bg="blue" color="white" mt="15%">
+              Try Whitespace free
+            </Button>
+          </Stack>
+
+          <Box>
+            <Image
+              src="https://images.unsplash.com/photo-1556745757-8d76bdb6984b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+              w="50%"
+              float="right"
+            />
+          </Box>
+        </HStack>
       </Flex>
     </>
   );
--- src/MainPage/LandingPage.jsx
+++ src/MainPage/LandingPage.jsx
@@ -3,14 +3,14 @@ import Header from "./Header";
 import { Flex, Stack } from "@chakra-ui/react";
 import HeroSection from "./HeroSection";
 
-export default function () {
+export default function LandingPage() {
   return (
     <>
       <Stack>
         <Flex position="fixed" top="0" left="0" zIndex="1000">
           <Header />
         </Flex>
-        <Flex position="center">
+        <Flex position="">
           <HeroSection />
         </Flex>
       </Stack>
